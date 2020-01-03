bdd07be3d6c9119b2a8ea66ffda70fbff0da3b17
Lassi Eloranta <lassi.eloranta@gmail.com>
2020-01-03 15:01:54 +0200

    Initial working version.

--- creategamelist.py
+++ creategamelist.py
@@ -1,3 +1,26 @@
 import glob
 import os
-import argparse
+import sys
+import subprocess
+
+if len(sys.argv) > 2:
+    print("Too many arguments.")
+    sys.exit()
+
+if len(sys.argv) < 2:
+    rompath = os.getcwd()
+    print(rompath)
+else:
+    if not os.path.isdir(sys.argv[1]):
+        print("The path you specified doesn't exist.")
+        sys.exit()
+    rompath = sys.argv[1]
+    print(rompath)
+
+with open("gameslist.txt", "a") as output:
+    for file in os.listdir(rompath):
+        if file.endswith(".zip"):
+            print(os.path.join(file))
+            output.write(file+"\n")
+
+os.system("sort gameslist.txt -o gameslist.txt")
