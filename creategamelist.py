cc211e842e5a466fcd1d6a34fb12c84824c6f095
Lassi Eloranta <lassi.eloranta@gmail.com>
2020-01-03 15:52:27 +0200

    Add some pathlib functionality as a test and add basic error handling.

--- creategamelist.py
+++ creategamelist.py
@@ -2,25 +2,26 @@ import glob
 import os
 import sys
 import subprocess
+from pathlib import Path
 
 if len(sys.argv) > 2:
     print("Too many arguments.")
     sys.exit()
 
 if len(sys.argv) < 2:
-    rompath = os.getcwd()
-    print(rompath)
+    rompath = Path.cwd()
 else:
-    if not os.path.isdir(sys.argv[1]):
+    if not Path(sys.argv[1]).exists:
         print("The path you specified doesn't exist.")
         sys.exit()
     rompath = sys.argv[1]
-    print(rompath)
 
-with open("gameslist.txt", "a") as output:
-    for file in os.listdir(rompath):
-        if file.endswith(".zip"):
-            print(os.path.join(file))
-            output.write(file+"\n")
-
-os.system("sort gameslist.txt -o gameslist.txt")
+try:
+    with open("gameslist.txt", "a") as output:
+        for file in os.listdir(rompath):
+            if file.endswith(".zip"):
+                print(os.path.join(file))
+                output.write(file+"\n")
+    os.system("sort gameslist.txt -o gameslist.txt")
+except Exception:
+    print("Unexpected error:", sys.exc_info()[0])
