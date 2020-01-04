58b1e41db55c634d3a90b7cc74e9657cf54fe320
Lassi Eloranta <lassi.eloranta@gmail.com>
2020-01-05 01:24:28 +0200

    Implement the possibility to backup the current gameslist-file

--- creategamelist.py
+++ creategamelist.py
@@ -5,6 +5,7 @@ import os
 import sys
 import subprocess
 from pathlib import Path
+from datetime import datetime
 
 if len(sys.argv) > 2:
     print("Too many arguments.")
@@ -18,12 +19,30 @@ else:
         sys.exit()
     rompath = sys.argv[1]
 
+gamefile = Path("gameslist.txt")
+date = datetime.today().strftime("%d-%m-%Y")
+
 try:
-    with open("gameslist.txt", "a") as output:
+    if gamefile.is_file():
+        print("There's already a gameslist-file, do you want to make a backup of it?")
+        answer = input("Y/N?\n")
+        while(answer != "Y" and answer != "N"):
+            print("Only accepting Y for yes and N for no.")
+            answer = input("Y/N?\n")
+        if(answer == "Y"):
+            gamefile.rename(
+                Path(gamefile.parent, f"{gamefile.stem}_{date}" + gamefile.suffix))
+        elif(answer == "N"):
+            gamefile.unlink()
+except Exception:
+    print("Unexpected error:", sys.exc_info()[0])
+
+try:
+    with open(gamefile, "a") as output:
         for file in os.listdir(rompath):
             if file.endswith(".zip"):
                 print(os.path.join(file))
                 output.write(file+"\n")
-    os.system("sort gameslist.txt -o gameslist.txt")
+    os.system(f"sort {gamefile} -o {gamefile}")
 except Exception:
     print("Unexpected error:", sys.exc_info()[0])
