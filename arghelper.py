84eda65501df020fb2345c9fd0280b090c6fcb3d
Lassi Eloranta <lassi.eloranta@gmail.com>
2020-01-05 01:47:43 +0200

    Move commandline args handling to a separate file & function

---
+++ arghelper.py
@@ -0,0 +1,17 @@
+import sys
+from pathlib import Path
+
+
+def get_rompath():
+    if len(sys.argv) > 2:
+        print("Too many arguments.")
+        sys.exit()
+
+    if len(sys.argv) < 2:
+        rompath = Path.cwd()
+    else:
+        if not Path(sys.argv[1]).exists:
+            print("The path you specified doesn't exist.")
+            sys.exit()
+        rompath = sys.argv[1]
+    return rompath
