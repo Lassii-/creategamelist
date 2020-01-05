be9ca8069a2f8c01d24424cab599d5293017dfdd
Lassi Eloranta <lassi.eloranta@gmail.com>
2020-01-05 04:15:18 +0200

    Change filtergames so that it doesn't create the folder for results if the list needed for filtering isn't found

--- filtergames.py
+++ filtergames.py
@@ -5,15 +5,16 @@ import sys
 import arghelper
 from pathlib import Path
 
+gamefile = Path("gameslist.txt")
 rompath = arghelper.get_rompath()
-Path(rompath / "result").mkdir(parents=True, exist_ok=True)
-output = rompath / "result"
 
 try:
-    if Path("gameslist.txt").is_file:
-        with open("gameslist.txt") as gameslist:
-            for game in gameslist:
-                os.rename(os.path.join(rompath, game.strip()),
-                          output / game.strip())
-except OSError as e:
+    with open(gamefile) as gameslist:
+        Path(rompath / "result").mkdir(parents=True, exist_ok=True)
+        output = rompath / "result"
+        for game in gameslist:
+            os.rename(os.path.join(rompath, game.strip()),
+                      output / game.strip())
+except (OSError, FileNotFoundError) as e:
+    print("If you got an error saying gameslist.txt doesn't exist, you probably forgot to run creategamelist.py first.")
     print(e)
