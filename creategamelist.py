2eb711ad652b8b414c052269d305470dee5c805b
Lassi Eloranta <lassi.eloranta@gmail.com>
2020-07-29 17:37:01 +0300

    Add support for other platforms than MAME

--- creategamelist.py
+++ creategamelist.py
@@ -9,8 +9,32 @@ from pathlib import Path
 from datetime import datetime
 
 rompath = arghelper.get_rompath()
-gamefile = Path("gameslist.txt")
+
 date = datetime.today().strftime("%d-%m-%Y")
+answer = input("Which platform? (MAME/NES/SNES/GB/GBC/GBA/N64/PCE/MD ")
+while(answer != "MAME" and answer != "NES" and answer != "SNES" and answer != "GB" and answer != "GBC" and answer != "GBA" and answer != "N64" and answer != "PCE" and answer != "MD"):
+    print("You didn't pick a correct platform! Choose from MAME/NES/SNES/GB/GBC/N64/PCE/MD")
+    answer = input("Which platform? ")
+
+if(answer == "MAME"):
+    gamefile = Path("MAMEgameslist.txt")
+elif(answer == "NES"):
+    gamefile = Path("NESgameslist.txt")
+elif(answer == "SNES"):
+    gamefile = Path("SNESgameslist.txt")
+elif(answer == "GB"):
+    gamefile = Path("GBgameslist.txt")
+elif(answer == "GBC"):
+    gamefile = Path("GBCgameslist.txt")
+elif(answer == "GBA"):
+    gamefile = Path("GBAgameslist.txt")
+elif(answer == "N64"):
+    gamefile = Path("N64gameslist.txt")
+elif(answer == "PCE"):
+    gamefile = Path("PCEgameslist.txt")
+elif(answer == "MD"):
+    gamefile = Path("MDgameslist.txt")
+
 
 try:
     if gamefile.is_file():
