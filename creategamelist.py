30d58c819149ce1385156817ff7127fec9229b9f
Lassi Eloranta <lassi.eloranta@gmail.com>
2021-01-20 19:54:33 +0200

    add main functions

--- creategamelist.py
+++ creategamelist.py
@@ -10,28 +10,32 @@ rompath: Path = arghelper.get_rompath()
 gamefile: Path = arghelper.get_gameslist()
 date = datetime.today().strftime("%d-%m-%Y")
 
-try:
-    if gamefile.is_file():
-        print(f"There's already a {gamefile}-file, do you want to make a backup of it?")
-        answer = input("Y/N?\n").upper()
-        while(answer != "Y" and answer != "N"):
-            print("Only accepting Y for yes and N for no.")
-            answer = input("Y/N?\n")
-        if(answer == "Y"):
-            # Rename the old file with current date appended to the filename
-            gamefile.rename(
-                Path(gamefile.parent, f"{gamefile.stem}_{date}" + gamefile.suffix))
-        elif(answer == "N"):
-            # Remove the old file
-            gamefile.unlink()
-except OSError as e:
-    print(e)
+def main():
+    try:
+        if gamefile.is_file():
+            print(f"There's already a {gamefile}-file, do you want to make a backup of it?")
+            answer = input("Y/N?\n").upper()
+            while(answer != "Y" and answer != "N"):
+                print("Only accepting Y for yes and N for no.")
+                answer = input("Y/N?\n")
+            if(answer == "Y"):
+                # Rename the old file with current date appended to the filename
+                gamefile.rename(
+                    Path(gamefile.parent, f"{gamefile.stem}_{date}" + gamefile.suffix))
+            elif(answer == "N"):
+                # Remove the old file
+                gamefile.unlink()
+    except OSError as e:
+        print(e)
 
-try:
-    with open(gamefile, "a") as output:
-        for file in sorted(os.listdir(rompath)):
-            if file.endswith(".zip"):
-                print(os.path.join(file))
-                output.write(file+"\n")
-except OSError as e:
-    print(e)
+    try:
+        with open(gamefile, "a") as output:
+            for file in sorted(os.listdir(rompath)):
+                if file.endswith(".zip"):
+                    print(os.path.join(file))
+                    output.write(file+"\n")
+    except OSError as e:
+        print(e)
+
+if __name__ == '__main__':     # Runs main() if file wasn't imported.
+    main()
--- filtergames.py
+++ filtergames.py
@@ -7,13 +7,17 @@ from pathlib import Path
 rompath: Path = arghelper.get_rompath()
 gamefile: Path = arghelper.get_gameslist()
 
-try:
-    with open(gamefile) as gameslist:
-        Path(rompath / "result").mkdir(parents=True, exist_ok=True)
-        output = rompath / "result"
-        for game in gameslist:
-            os.rename(os.path.join(rompath, game.strip()),
-                      output / game.strip())
-except (OSError, FileNotFoundError) as e:
-    print(e)
-    print(f"If you got an error saying {gamefile}.txt doesn't exist, you probably forgot to run creategamelist.py first.")
+def main():
+    try:
+        with open(gamefile) as gameslist:
+            Path(rompath / "result").mkdir(parents=True, exist_ok=True)
+            output = rompath / "result"
+            for game in gameslist:
+                os.rename(os.path.join(rompath, game.strip()),
+                        output / game.strip())
+    except (OSError, FileNotFoundError) as e:
+        print(e)
+        print(f"If you got an error saying {gamefile}.txt doesn't exist, you probably forgot to run creategamelist.py first.")
+
+if __name__ == '__main__':
+    main()
